176944f368d48c3eaceb192434e0a0241cb98990
Westbot657 <Westbot657@gmail.com>
2022-04-05 16:04:03 -0600

    Started adding emergency stop

--- src/robot_colors_nano/robot_colors_nano.ino
+++ src/robot_colors_nano/robot_colors_nano.ino
@@ -12,7 +12,7 @@
 #define LED_PIN    6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 30
+#define LED_COUNT 208
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -69,6 +69,28 @@ void colorBlink(uint32_t c1, uint32_t c2, uint8_t wait=100) {
   colorSolid(c2, wait);
 }
 
+void lineCrawl(uint32_t c1, uint32_t c2) {
+  int x = 0;
+  for (int j=0; j<5; j++) {
+    x = j;
+    for (int i=0; i < strip.numPixels(); i++) {
+      if (0 <= x && x <= 2) {
+        strip.setPixelColor(i, c1);
+      }
+      else {
+        strip.setPixelColor(i, c2);
+      }
+
+      x += 1;
+      if (x == 5) {
+        x = 0;
+      }
+    }
+    strip.show();
+    delay(100);
+  }
+}
+
 void colorAlternate(uint32_t c1, uint32_t c2) {
   byte m = 0;
   byte p = 0;
@@ -115,55 +137,80 @@ void randomized(uint8_t r, uint8_t g, uint8_t b, uint8_t range) {
   strip.show();
 }
 
+uint8_t mode = 0;
 uint8_t state = 0;
 uint8_t analog = 1;
 unsigned long duration = 0;
 
-void check_analog() {
+boolean check_analog() {
   uint16_t value = analogRead(2);
 
   Serial.println(value);
 
+  // value = 2;
+
   uint8_t v = 26;
-  if (0 <= value && value <= v) { state = 0; }
-  else if ((v)+2 <= value && value <= v*2) { state = 1; }
-  else if ((v*2)+2 <= value && value <= v*3) { state = 2; }
-  else if ((v*3)+2 <= value && value <= v*4) { state = 3; }
-  else if ((v*4)+2 <= value && value <= v*5) { state = 4; }
-  else if ((v*5)+2 <= value && value <= v*6) { state = 5; }
-  else if ((v*6)+2 <= value && value <= v*7) { state = 6; }
-  else if ((v*7)+2 <= value && value <= v*8) { state = 7; }
-  else if ((v*8)+2 <= value && value <= v*9) { state = 8; }
-  else if ((v*9)+2 <= value && value <= v*10) { state = 9; }
-  else if ((v*10)+2 <= value && value <= v*11) { state = 10; }
-  else if ((v*11)+2 <= value && value <= v*12) { state = 11; }
-  else if ((v*12)+2 <= value && value <= v*13) { state = 12; }
-  else if ((v*13)+2 <= value && value <= v*14) { state = 13; }
-  else if ((v*14)+2 <= value && value <= v*15) { state = 14; }
-  else if ((v*15)+2 <= value && value <= v*16) { state = 15; }
+
+  //if (value >= ) { // kill arduino in case of electrical short
+  //  return true;
+  //}
+  if (0 <= value && value <= v) { state = 0; } // green alt
+  else if ((v)+2 <= value && value <= v*2) { state = 1; } // red alt
+  else if ((v*2)+2 <= value && value <= v*3) { state = 2; } // blue alt
+  else if ((v*3)+2 <= value && value <= v*4) { state = 3; } // yellow alt
+  else if ((v*4)+2 <= value && value <= v*5) { state = 4; } // green blink
+  else if ((v*5)+2 <= value && value <= v*6) { state = 5; } // red blink
+  else if ((v*6)+2 <= value && value <= v*7) { state = 6; } // blue blink
+  else if ((v*7)+2 <= value && value <= v*8) { state = 7; } // yellow blink
+  else if ((v*8)+2 <= value && value <= v*9) { state = 8; } // green solid
+  else if ((v*9)+2 <= value && value <= v*10) { state = 9; } // red solid
+  else if ((v*10)+2 <= value && value <= v*11) { state = 10; } // blue solid
+  else if ((v*11)+4 <= value && value <= v*13) { state = 11; } // yellow solid
+  else if ((v*13)+4 <= value && value <= v*15) { state = 12; } // blue/green
+  else if ((v*15)+2 <= value && value <= v*17) { state = 13; } // blue/red
+  else if ((v*17)+2 <= value && value <= v*19) { state = 14; } // green/red
+  else if ((v*19)+2 <= value && value <= v*21) { state = 15; } // randomized
+
+  return false;
 }
 
 
 void loop() {
 
-  check_analog();
-
-  switch (state) {
-    case 0:  { colorAlternate(green, off); break; }
-    case 1:  { colorAlternate(red, off); break; }
-    case 2:  { colorAlternate(blue, off); break; }
-    case 3:  { colorAlternate(yellow, off); break; }
-    case 4:  { colorBlink(green, off); break; }
-    case 5:  { colorBlink(red, off); break; }
-    case 6:  { colorBlink(blue, off); break; }
-    case 7:  { colorBlink(yellow, off); break; }
-    case 8:  { colorSolid(green); break; }
-    case 9:  { colorSolid(red); break; }
-    case 10: { colorSolid(blue); break; }
-    case 11: { colorSolid(yellow); break; }
-    case 12: { colorAlternate(blue, green); break; }
-    case 13: { colorAlternate(blue, red); break; }
-    case 14: { colorAlternate(green, red); break; }
-    case 15: { randomized(-5, 250, 0, 5); }
+  boolean a = check_analog();
+  if (a) {exit(0); return;} // in case of electical short
+  switch(mode) {
+    case 0: {
+      switch (state) {
+        case 0:  { colorAlternate(green, off); break; }
+        case 1:  { colorAlternate(red, off); break; }
+        case 2:  { colorAlternate(blue, off); break; }
+        case 3:  { colorAlternate(yellow, off); break; }
+        case 4:  { colorBlink(green, off); break; }
+        case 5:  { colorBlink(red, off); break; }
+        case 6:  { colorBlink(blue, off); break; }
+        case 7:  { colorBlink(yellow, off); break; }
+        case 8:  { colorSolid(green); break; }
+        case 9:  { colorSolid(red); break; }
+        case 10: { colorSolid(blue); break; }
+        case 11: { colorSolid(yellow); break; }
+        case 12: { colorAlternate(blue, green); break; }
+        case 13: { colorAlternate(blue, red); break; }
+        case 14: { colorAlternate(green, red); break; }
+        case 15: { randomized(-5, 250, 0, 5); }
+      }
+      break;
+    }
+    case 1: {
+      switch (state) {
+        case 0:  { colorAlternate(green, off); break; }
+        case 1:  { lineCrawl(yellow, off); break; }
+        case 2:  { lineCrawl(blue, off); break; }
+        case 3:  { lineCrawl(red, off); break; }
+        case 4:  { colorBlink(red, off); break; }
+        case 5:  { colorBlink(green, off); break; }
+        case 6:  { colorBlink(blue, off); break; }
+      }
+    }
   }
 }
